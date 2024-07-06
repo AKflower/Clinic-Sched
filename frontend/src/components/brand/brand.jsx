ebf539225284cc4c3a0fd98654be9e91fa1ed857
AKflower <anhkhoabqv@gmail.com>
2024-07-06 15:09:35 +0700

    [Feat] Add frontend

---
+++ frontend/src/components/brand/brand.jsx
@@ -0,0 +1,1 @@
+import styles from './brand.module.scss'
---
+++ frontend/src/pages/Login/login.jsx
@@ -0,0 +1,9 @@
+import styles from './login.module.scss'
+
+export default function Login () {
+    return (
+        <div className="login-container">
+            <div className='brand-container'></div>
+        </div>
+    )
+}
