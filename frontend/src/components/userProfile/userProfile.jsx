6f632211858af5b0ffa258ba9a19a3b72ba91129
AKflower <anhkhoabqv@gmail.com>
2024-07-07 16:07:58 +0700

    [Feat] Logout Icon

--- frontend/src/components/userProfile/userProfile.jsx
+++ frontend/src/components/userProfile/userProfile.jsx
@@ -1,5 +1,6 @@
 import styles from './userProfile.module.scss';
 import {useLocation} from 'react-router-dom'
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function UserProfile () {
     const path = useLocation().pathname;
@@ -10,6 +11,7 @@ export default function UserProfile () {
         {!(path.includes('/login') || path.includes('/register') || path.includes('/forgot')) && <div className={styles.container}>
             <div className={styles.avatar}></div>
             <div className={styles.name}>Nguyễn Văn A</div>
+            <div style={{cursor: 'pointer'}}><LogoutIcon /></div>
         </div>}
         </>
     )
