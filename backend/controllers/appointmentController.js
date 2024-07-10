d2619c02cd69d688f33c8c68e185f912bffb320c
Trần Quốc Dũng <78058950+danisolation@users.noreply.github.com>
2024-07-10 15:50:28 +0700

    [Fix]: fix createRoom

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,5 +1,5 @@
 const Appointment = require('../models/Appointment');
-const User = require('../models/User');
+const User = require('../models/user');
 const Doctor = require('../models/doctor');
 const mongoose = require('mongoose');
 
